d2e974ff6477a7aad88f3f7e1853b8b53e075e84
komi1230 <yukke.konan@gmail.com>
2023-01-11 14:52:11 +0900

    .

---
+++ components/layout.tsx
@@ -0,0 +1,22 @@
+
+import Footer from './footer'
+import Meta from './meta'
+
+type Props = {
+  preview?: boolean
+  children: React.ReactNode
+}
+
+const Layout = ({ preview, children }: Props) => {
+  return (
+    <>
+      <Meta />
+      <div className="min-h-screen container mx-auto px-5">
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
