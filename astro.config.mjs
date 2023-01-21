55a474ad19bd677f82c60854ee49059c6cfd2705
h3y6e <h3y6e@pm.me>
2023-01-22 08:11:43 +0900

    format all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -13,6 +13,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ksquad.jp',
-  integrations: [mdx(), sitemap(), tailwind(), image(), preact()]
+  site: "https://ksquad.jp",
+  integrations: [mdx(), sitemap(), tailwind(), image(), preact()],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import rss from '@astrojs/rss';
-import { options } from 'preact';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import rss from "@astrojs/rss";
+import { options } from "preact";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
-const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
+const postImportResult = import.meta.glob("./blog/**/*.md", { eager: true });
 const posts = Object.values(postImportResult);
 
 export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			title: post.frontmatter.title,
-			pubDate: post.frontmatter.date,
-			link: post.url,
-		})),
-	});
+  rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.date,
+      link: post.url,
+    })),
+  });
