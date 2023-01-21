4337c1d1a3b7d201525182fae424d3b5d2cc9aa4
Yusuke Kominami <yukke.konan@gmail.com>
2023-01-21 16:52:20 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'K Squad';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'ケイスクワッド合同会社はソフトウェア技術を通してお客様へ最大価値を提供します。アプリ開発からWebシステム構築、データ基盤整備を一気通貫で対応します。';
