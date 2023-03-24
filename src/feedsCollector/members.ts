83e6b2cfcbcfba530a866d51ac12fe8cefe5b3fb
umepon0626 <ryuichiastrona@gmail.com>
2023-03-24 16:48:23 +0900

    chore: fix members

--- src/feedsCollector/members.ts
+++ src/feedsCollector/members.ts
@@ -2,14 +2,14 @@ import type { Member } from "./types";
 
 export const members: Member[] = [
   {
-    id: "Yusuke Kominami",
-    name: "小南 佑介",
-    role: "代表社員",
-    bio: "2020年京都大学理学部卒業。同年ソフトバンク株式会社に入社し、フルスタックエンジニアとして開発に参画。2021年にヘイ株式会社（後にSTORES株式会社に商号変更）に入社し、1人目のデータエンジニアとしてデータ基盤を構築。",
-    avatarSrc: "/avatars/catnose.jpg",
-    sources: ["https://komi.dev/index.xml"],
-    twitterUsername: "@komi_edtr_1230",
-    githubUsername: "komi1230",
-    websiteUrl: "https://komi.dev",
+    id: "Ryuichi Umehara",
+    name: "梅原 隆一",
+    role: "業務委託メンバー",
+    bio: "京都大学情報学研究科M1",
+    avatarSrc: "https://pbs.twimg.com/profile_images/1307941138297311232/McTtMOUI_200x200.jpg",
+    sources: ["https://qiita.com/ryuichiastrona/feed"],
+    twitterUsername: "@astrona0626",
+    githubUsername: "umepon0626",
+    websiteUrl: "https://qiita.com/ryuichiastrona",
   },
 ];
