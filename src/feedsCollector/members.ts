96eb35ca33d6001cbf6e72831ee568c43165d432
umepon0626 <ryuichiastrona@gmail.com>
2023-03-24 15:14:06 +0900

    :sparkles: migrate feedsCollector from original implementations (https://github.com/catnose99/team-blog-hub)

---
+++ src/feedsCollector/members.ts
@@ -0,0 +1,15 @@
+import type { Member } from "./types";
+
+export const members: Member[] = [
+  {
+    id: "Yusuke Kominami",
+    name: "小南 佑介",
+    role: "代表社員",
+    bio: "2020年京都大学理学部卒業。同年ソフトバンク株式会社に入社し、フルスタックエンジニアとして開発に参画。2021年にヘイ株式会社（後にSTORES株式会社に商号変更）に入社し、1人目のデータエンジニアとしてデータ基盤を構築。",
+    avatarSrc: "/avatars/catnose.jpg",
+    sources: ["https://komi.dev/index.xml"],
+    twitterUsername: "@komi_edtr_1230",
+    githubUsername: "komi1230",
+    websiteUrl: "https://komi.dev",
+  },
+];
