f2bd0967bee500ed5a4e2cae6c62680df0d89996
umepon0626 <ryuichiastrona@gmail.com>
2023-03-30 22:30:51 +0900

    :wrench: fix the members

--- src/feedsCollector/members.ts
+++ src/feedsCollector/members.ts
@@ -2,17 +2,6 @@ import type { Member } from "@types";
 
 export const members: Member[] = [
   {
-    id: "Ryuichi Umehara",
-    name: "梅原 隆一",
-    role: "業務委託メンバー",
-    bio: "京都大学情報学研究科M1",
-    avatarSrc: "https://pbs.twimg.com/profile_images/1307941138297311232/McTtMOUI_200x200.jpg",
-    sources: ["https://qiita.com/ryuichiastrona/feed"],
-    twitterUsername: "@astrona0626",
-    githubUsername: "umepon0626",
-    websiteUrl: "https://qiita.com/ryuichiastrona",
-  },
-  {
     id: "Ksquad Official",
     name: "Ksquad",
     role: "公式",
