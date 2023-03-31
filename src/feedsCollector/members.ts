ded77bde019cebdbfcc86e365683e6b19d9aa912
umepon0626 <ryuichiastrona@gmail.com>
2023-03-31 15:59:05 +0900

    :bug: fix tmp

--- src/feedsCollector/members.ts
+++ src/feedsCollector/members.ts
@@ -10,4 +10,15 @@ export const members: Member[] = [
     websiteUrl: "https://ksquad.jp",
     sources: ["https://ksquad.jp/rss.xml"],
   },
+  {
+    id: "Ryuichi Umehara",
+    name: "梅原 隆一",
+    role: "業務委託メンバー",
+    bio: "京都大学情報学研究科M1",
+    avatarSrc: "https://pbs.twimg.com/profile_images/1307941138297311232/McTtMOUI_200x200.jpg",
+    sources: ["https://qiita.com/ryuichiastrona/feed"],
+    twitterUsername: "@astrona0626",
+    githubUsername: "umepon0626",
+    websiteUrl: "https://qiita.com/ryuichiastrona",
+  },
 ];
