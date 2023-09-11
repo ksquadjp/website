fa4a3400099df7656cf52984d885a8fb672b0348
komi1230 <yukke.konan@gmail.com>
2023-09-11 11:11:02 +0900

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import preact from "@astrojs/preact";
 import partytown from "@astrojs/partytown";
 
@@ -11,10 +10,6 @@ export default defineConfig({
   integrations: [
     sitemap(),
     tailwind(),
-    image({
-      cacheDir: "./node_modules/.cache/image",
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     preact(),
     partytown({
       // Adds dataLayer.push as a forwarding-event.
