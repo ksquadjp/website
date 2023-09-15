cf6d2615aaea457f3507bc4c6956d68c3f86c6f8
komi1230 <yukke.konan@gmail.com>
2023-09-15 18:45:44 +0900

    Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,11 +10,5 @@ export default defineConfig({
     sitemap(),
     tailwind(),
     preact(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
 });
