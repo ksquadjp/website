cdcb4711a0ce1aa2d6c63393b2aa214dc5e6c10e
komi1230 <yukke.konan@gmail.com>
2023-09-16 15:58:03 +0900

    Triage

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const SITE_TITLE = "K Squad | トップ1%エンジニアによる技術�
 export const SITE_DESCRIPTION =
   "ケイスクワッド合同会社は技術顧問とITコンサルティングを行うプロフェッショナル集団です。業界トップクラスのエンジニアがお客さまのWebに関するお困りごとを解決します。開発から広告、エンジニア採用まで全方位的にサポートしますのでお気軽にお問い合わせください。";
 export const CONTACT_POST_URL =
-  "https://script.google.com/macros/s/AKfycbwP03CjlkpUKzC5O1Pq37j9iKjnMDJhZ9xAzTt5-Pc_qyOptVt3Nx-Jf-9Xu29RBFdU/exec";
+  "https://script.google.com/macros/s/AKfycbzXyq2bXqZYabiLut-BF85iJSuY_KKV_lxOzNiVGUIvmAX5XoCJbN8bNwDpu8sPxhmH/exec";
