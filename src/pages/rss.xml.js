0fed4870d09e8a1fe4c6c8183abf0e399979426f
komi1230 <komi@ksquad.jp>
2023-11-24 15:59:57 +0900

    Fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: "ブログトップ | K Squad ケイスクワッド株式会社",
+    description: "K Squadの知見や経験をブログとして発信しています。",
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
