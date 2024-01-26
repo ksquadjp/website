5f056f1b12885d63cd92fbdcd8da17d0eda470a2
yutorse <yutohorse.coc@gmail.com>
2024-01-26 17:51:02 +0900

    pnpm check が通るように修正

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,6 +1,5 @@
 import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
-import type { Blog } from "../content/config";
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
